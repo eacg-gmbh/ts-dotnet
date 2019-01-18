75c9ddc9d4efc3970a05ca2033e67627a7f991c9
JTH <git4jth@eacg.de>
2019-01-18 09:58:30 +0100

    Hello World incl. 3 dependencies as a sample

---
+++ TS-DotNetCore2-2-Sample/TS-DotNetCore2-2-Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TS_DotNetCore2_2_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
